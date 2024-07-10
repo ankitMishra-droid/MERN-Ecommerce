bd2fdfbff6c164bff0a37071214a45b3eb0bd889
Ankit Mishra <88356934+ankitMishra-droid@users.noreply.github.com>
2024-07-10 18:12:30 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,12 +21,4 @@ import userRouter from "./routes/user.routes.js";
 
 app.use("/api/users", userRouter)
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-app.use(express.static(path.join(__dirname, "../../frontend/build")))
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"))
-})
-
 export {app}
