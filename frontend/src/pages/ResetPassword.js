1007745825cbf640729e7416790574bbb8e16335
Ankit Mishra <88356934+ankitMishra-droid@users.noreply.github.com>
2024-08-06 17:12:37 +0530

    Update ResetPassword.js

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -56,9 +56,11 @@ const ResetPassword = () => {
               Set Password
             </label>
             <input
-              type="passsword"
+              type="password"
               id="password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter Password"
               required
