6cb6b63f2701958c708fd729bf547e0ac5c1a150
mishraankit9653@gmail.com <mishraankit9653@gmail.com>
2024-09-24 18:07:58 +0530

    update

--- frontend/src/components/VerticalProductCard.js
+++ frontend/src/components/VerticalProductCard.js
@@ -74,7 +74,7 @@ const VerticalProductCard = ({ loading, data = [] }) => {
                     </button>
                   </div> */}
 
-                  <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700">
                     <Link to="#">
                       <img
                         class="p-2 h-48 rounded-t-lg mx-auto"
